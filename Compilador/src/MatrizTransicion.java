a9acb01caf2ffac4ddb11cdba6139736d3a23cd9
Tomas E. Schuenemann <TomasSchuenemann@outlook.com>
2022-09-12 17:36:00 -0300

    Hay que implementar el map

--- Compilador/src/MatrizTransicion.java
+++ Compilador/src/MatrizTransicion.java
@@ -1,15 +1,26 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MatrizTransicion {
     private int matrizEstado [][];
     private int row;
     private int col;
     //Se puede implementar los simbolos con un hash, la idea es buscar la columna dependeindo el simbolo.
+    private Map simbolos;
 
     public MatrizTransicion(int row, int col) {
         this.matrizEstado = new int[row][col];
+        this.simbolos = new HashMap<Character, Integer>();
     }
 
     public void generarMatriz(){
         // Se completan los datos de la matriz de transicion
+
+    }
+    public void cargarSimbolos(){
+        // hay que completar el hashmap para identificar los simbolos especias
+        // .	<	>	!	"="	"+"	"-"	"*"	"/"	{	}	[	]	(	)	,	;	:	´	"_"	$
+        // la idea de usar este map, es que dado el simbolo que entro nos devuelva la columna en el estado 0;
     }
 
     public void leerCaracterArchivo(char caracterArchivo){
