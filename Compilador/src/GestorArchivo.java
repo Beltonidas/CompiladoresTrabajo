4912d0916aaa13240663f58514510e1c11139ada
Tomas E. Schuenemann <TomasSchuenemann@outlook.com>
2022-09-12 16:01:01 -0300

    agrege las matrices y el gestor de archivos

--- Compilador/src/GestorArchivo.java
+++ Compilador/src/GestorArchivo.java
@@ -2,4 +2,25 @@ import java.io.FileReader;
 
 public class GestorArchivo {
     private FileReader fileReader;
+    private int value;
+
+    public GestorArchivo(FileReader fileReader){
+        this.fileReader = fileReader;
+    }
+
+    public void readCode(String urlArchivo){
+
+        try {
+            int value = fileReader.read();
+            while (value!= -1){
+                System.out.println((char)value);
+                value = fileReader.read();
+            }
+            fileReader.close();
+            
+        } catch (Exception e) {
+            System.out.println("No se pudo leer el archivo");
+        }
+    }
+
 }
